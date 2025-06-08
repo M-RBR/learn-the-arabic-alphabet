1023a37dae88f0bef5a61b4c2d3415528d574082
maximrubenbenjamin <roozenma@gmail.com>
2025-06-08 19:31:16 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import data from "./data/arabic_letters.json";
+import { useState } from "react";
+import FlipCards from "./Components/FlipCards";
+
+function App() {
+  return (
+    <div>
+      <h1>Learn the Arabic alphabet</h1>
+
+      <div>FlipCards</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/FlipCards.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import Modal from "./Modal";
+
+import React from "react";
+
+function FlipCards() {
+  return <div>FlipCards</div>;
+}
+
+export default FlipCards;
